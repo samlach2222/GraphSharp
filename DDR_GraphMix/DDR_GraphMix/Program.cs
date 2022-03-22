7bd8aba7e6732cdd4b54579826048cbad3737eaa
samlach2222 <lachaudsamuel@protonmail.com>
2022-03-22 20:05:37 +0100

    dégénérescence du graphe
    Co-Authored-By: Mahtwo <89837262+Mahtwo@users.noreply.github.com>

--- DDR_GraphMix/DDR_GraphMix/Program.cs
+++ DDR_GraphMix/DDR_GraphMix/Program.cs
@@ -8,6 +8,7 @@ namespace DDR_GraphMix
     class Program
     {
         static Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+        static Dictionary<int, int> vertexDegenerationTable;
 
         static void Main(string[] args)
         {
@@ -25,15 +26,22 @@ namespace DDR_GraphMix
                         int curNode = Int32.Parse(splitedLine[0]);
                         int nextNode = Int32.Parse(splitedLine[1]);
 
-                        insertion(curNode, nextNode); // insert ints into 
+                        Insert(curNode, nextNode); // insert ints into 
+                        Insert(nextNode, curNode); // insert ints into 
                     }  
                 }
             }
+            //DisplayGraph();
+            VertexDegenerationFilling();
 
-            displayGraph();
         }
 
-        static void insertion(int curNode, int nextNode)
+        /// <summary>
+        /// This function is used to insert values into the value dictionary 
+        /// </summary>
+        /// <param name="curNode"></param>
+        /// <param name="nextNode"></param>
+        static void Insert(int curNode, int nextNode)
         {
             if (!graph.ContainsKey(curNode)){ // if the current node is not in the keys
                 List<int> list = new List<int>();
@@ -46,7 +54,10 @@ namespace DDR_GraphMix
             }
         }
 
-        static void displayGraph()
+        /// <summary>
+        /// This function is used to display in the console the nodes connected to each point 
+        /// </summary>
+        static void DisplayGraph()
         {
             foreach(int key in graph.Keys) // for each keys
             {
@@ -58,6 +69,54 @@ namespace DDR_GraphMix
                 }
                 Console.WriteLine(line);
             }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static void VertexDegenerationFilling()
+        {
+            vertexDegenerationTable = new Dictionary<int, int>();
+            Dictionary<int, List<int>> localGraph = graph.ToDictionary(entry => entry.Key, entry => entry.Value);
+            int k = 1;
+
+            while(localGraph.Count != 0)
+            {
+                // filling a list with all key we have to delete
+                List<int> removeKeys = new List<int>();
+                foreach (int key in localGraph.Keys) // for each keys
+                {
+                    List<int> list = localGraph[key]; // get all nextNode of the key
+                    if (list.Count <= k)
+                    {
+                        removeKeys.Add(key);
+                    }
+                }
+
+                // Delete keys and delete values in nextnodes values
+                foreach (int key in removeKeys) // for each keys
+                {
+                    localGraph.Remove(key);
+                    foreach(int row in localGraph.Keys)
+                    {
+                        List<int> list = localGraph[row]; // get all nextNode of the key
+                        list.Remove(key);
+                    }
+                    vertexDegenerationTable.Add(key, k);
+                }
+
+                // 
+                if (removeKeys.Count == 0)
+                {
+                    k++;
+                }
+            }
+            
+            foreach(int key in vertexDegenerationTable.Keys.OrderBy(key => key))
+            {
+                Console.WriteLine(key + "\t" + vertexDegenerationTable[key]);
+            }
+            Console.WriteLine("Degeneration : " + k);
         }
     }
 }
