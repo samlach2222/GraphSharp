709ab3652bc85e859ee5d922e1898a08e1ed1e69
samlach2222 <lachaudsamuel@protonmail.com>
2022-03-28 12:56:16 +0200

    Début de la génération d'un pdf

--- DDR_GraphMix/DDR_GraphMix/Program.cs
+++ DDR_GraphMix/DDR_GraphMix/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata;
+using Document = iTextSharp.text.Document;
 
 namespace DDR_GraphMix
 {
@@ -28,12 +32,12 @@ namespace DDR_GraphMix
 
                         Insert(curNode, nextNode); // insert ints into 
                         Insert(nextNode, curNode); // insert ints into 
-                    }  
+                    }
                 }
             }
             //DisplayGraph();
-            VertexDegenerationFilling();
-
+            //VertexDegenerationFilling();
+            CreatePDF();
         }
 
         /// <summary>
@@ -43,7 +47,8 @@ namespace DDR_GraphMix
         /// <param name="nextNode"></param>
         static void Insert(int curNode, int nextNode)
         {
-            if (!graph.ContainsKey(curNode)){ // if the current node is not in the keys
+            if (!graph.ContainsKey(curNode))
+            { // if the current node is not in the keys
                 List<int> list = new List<int>();
                 list.Add(nextNode);
                 graph.Add(curNode, list);
@@ -59,13 +64,13 @@ namespace DDR_GraphMix
         /// </summary>
         static void DisplayGraph()
         {
-            foreach(int key in graph.Keys) // for each keys
+            foreach (int key in graph.Keys) // for each keys
             {
                 List<int> list = graph[key]; // get all nextNode of the key
                 string line = "Node : " + key + " --> \n";
-                foreach(int nextNode in list)
+                foreach (int nextNode in list)
                 {
-                        line += "\t" + nextNode + "\n";
+                    line += "\t" + nextNode + "\n";
                 }
                 Console.WriteLine(line);
             }
@@ -80,7 +85,7 @@ namespace DDR_GraphMix
             Dictionary<int, List<int>> localGraph = graph.ToDictionary(entry => entry.Key, entry => entry.Value);
             int k = 1;
 
-            while(localGraph.Count != 0)
+            while (localGraph.Count != 0)
             {
                 // filling a list with all key we have to delete
                 List<int> removeKeys = new List<int>();
@@ -97,7 +102,7 @@ namespace DDR_GraphMix
                 foreach (int key in removeKeys) // for each keys
                 {
                     localGraph.Remove(key);
-                    foreach(int row in localGraph.Keys)
+                    foreach (int row in localGraph.Keys)
                     {
                         List<int> list = localGraph[row]; // get all nextNode of the key
                         list.Remove(key);
@@ -111,12 +116,39 @@ namespace DDR_GraphMix
                     k++;
                 }
             }
-            
-            foreach(int key in vertexDegenerationTable.Keys.OrderBy(key => key))
+
+            foreach (int key in vertexDegenerationTable.Keys.OrderBy(key => key))
             {
                 Console.WriteLine(key + "\t" + vertexDegenerationTable[key]);
             }
             Console.WriteLine("Degeneration : " + k);
+        }
+
+        static void CreatePDF()
+        {
+            string pdfFile = @"Resources\export.pdf";
+
+            Document document = new Document(PageSize.A4, 10, 10, 10, 10);
+            FileStream fs = new FileStream(pdfFile, FileMode.Create, FileAccess.Write);
+            PdfWriter writer = PdfWriter.GetInstance(document, fs);
+            document.Open();
+
+            // the pdf content
+            PdfContentByte cb = writer.DirectContent;
+
+            cb.SetColorStroke(new BaseColor(183, 3, 223));
+            cb.Circle(document.GetRight(-10) / 2, document.GetTop(-10) / 2, 50);
+            cb.Stroke();
+            cb.SetColorStroke(new BaseColor(179, 1, 191));
+            cb.Circle(document.GetRight(-10) / 2, document.GetTop(-10) / 2, 100);
+            cb.Stroke();
+            cb.SetColorStroke(new BaseColor(242, 3, 255));
+            cb.Circle(document.GetRight(-10) / 2, document.GetTop(-10) / 2, 150);
+            cb.Stroke();
+
+            document.Close();
+            fs.Close();
+            writer.Close();
         }
     }
 }
